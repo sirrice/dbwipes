dcfcbc74a6ef16e49588420cd56dfd9bf01ee743
eugene wu <sirrice@gmail.com>
2014-05-12 18:34:25 -0400

    added server

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    import ez_setup
+    ez_setup.use_setuptools()
+from setuptools import setup, find_packages
+import summary
+
+setup(name="summary",
+      version=summary.__version__,
+      description="summarize a database",
+      license="MIT",
+      author="Eugene Wu",
+      author_email="eugenewu@mit.edu",
+      url="http://github.com/sirrice/summary",
+      include_package_data = True,      
+      packages = find_packages(),
+      package_dir = {'summary' : 'summary'},
+      scripts = ['summary/server.py'],
+      package_data = { 'dbtruck' : ['data/*'] },
+      install_requires = ['flask', 'psycopg2', 'sqlalchemy'],
+      keywords= "")
---
+++ summary/__init__.py
@@ -0,0 +1,3 @@
+from summary import *
+__all__ = ['summary']
+__version__ = '0.0.1'
