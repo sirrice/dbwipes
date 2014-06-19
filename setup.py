2f8edbca1f31efbe90c4ab5cc56bde27c4038735
eugene wu <sirrice@gmail.com>
2014-06-19 16:07:34 -0400

    added setup.cfg

--- setup.py
+++ setup.py
@@ -5,20 +5,20 @@ except ImportError:
     import ez_setup
     ez_setup.use_setuptools()
 from setuptools import setup, find_packages
-import summary
+import dbwipes
 
-setup(name="summary",
+setup(name="dbwipes",
       version=summary.__version__,
-      description="summarize a database",
+      description="dbwipes a database",
       license="MIT",
       author="Eugene Wu",
       author_email="eugenewu@mit.edu",
       url="http://github.com/sirrice/summary",
       include_package_data = True,      
       packages = find_packages(),
-      package_dir = {'summary' : 'summary'},
+      package_dir = {'dbwipes' : 'dbwipes'},
       scripts = [
-        'bin/summaryserver.py',
+        'bin/dbwipesserver.py',
         'bin/summarizedb.py'
       ],
       #zip_safe = False,
